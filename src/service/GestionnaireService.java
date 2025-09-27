5b466fb8a7739f21d5889898a59eb15cb85a08eb
ilyass bourass <ilyass.b.1992@gmail.com>
2025-09-27 22:57:02 +0100

    feat:imlémentation de view pour le client

--- src/service/GestionnaireService.java
+++ src/service/GestionnaireService.java
@@ -11,8 +11,7 @@ public class GestionnaireService {
     }
 
     public void ajouterClient(String nom, String prenom, String email, String motPasse) {
-        // استخدام طريقة التسجيل الموجودة في AuthService
-        // هذا سيضمن التحقق من صحة البيانات ومن عدم وجود email مكرر
+        
         authService.inscrireClient(nom, prenom, email, motPasse);
     }
 
--- src/view/ClientView.java
+++ src/view/ClientView.java
@@ -18,10 +18,13 @@ public class ClientView {
             System.out.println("║            ESPACE CLIENT                   ║");
             System.out.println("╠════════════════════════════════════════════╣");
             System.out.println("║ 1. Voir mes informations                   ║");
-            System.out.println("║ 2. faire un virement                       ║");
-            System.out.println("║ 3. Consulter mes transactions              ║");
-            System.out.println("║ 4. Ajouter un bénéficiaire                 ║");
-            System.out.println("║ 5. Déconnexion                             ║");
+            System.out.println("║ 2. Consulter mes comptes bancaires         ║");
+            System.out.println("║ 3. Historique de mes transactions          ║");
+            System.out.println("║ 4. Filtrer mes transactions                ║");
+            System.out.println("║ 5. le solde total                          ║");
+            System.out.println("║ 6. les montants total déposés              ║");
+            System.out.println("║ 7. les montants total retirés              ║");
+            System.out.println("║ 8. Déconnexion                             ║");
             System.out.println("╚════════════════════════════════════════════╝");
             System.out.print("Choisissez une option: ");
 
@@ -32,21 +35,44 @@ public class ClientView {
                     System.out.println("=== Mes informations ===");
                     break;
                 case 2:
-                    System.out.println("=== Faire un virement ===");
+                    System.out.println("=== Consulter mes comptes bancaires ===");
                     break;
                 case 3:
-                    System.out.println("=== Mes transactions ===");
+                    System.out.println("=== Historique de mes transactions ===");
                     break;
                 case 4:
-                    System.out.println("=== Ajouter un bénéficiaire ===");
+                    System.out.println("=== Filtrer mes transactions ===");
+                    System.out.println("1 : Par type");
+                    System.out.println("1 : Par date");
+                    System.out.println("1 : Par montant");
+                    System.out.println("choisisez votre choix");
+                    int choixFiltre = sc.nextInt();
+                    if(choixFiltre == 1){
+                        System.out.println("=== Filtrer par type ===");
+                    } else if (choixFiltre == 2){
+                        System.out.println("=== Filtrer par date ===");
+                    } else if (choixFiltre == 3){
+                        System.out.println("=== Filtrer par montant ===");
+                    } else {
+                        System.out.println("Choix invalide. Veuillez réessayer.");
+                    }
                     break;
                     
                 case 5:
-                    System.out.println("Au revoir!");
-                    continuer = false;
+                    System.out.println("=== le solde total ===");
                     break;
+                case 06:
+                    System.out.println("=== les montants total déposés ===");
+                    break;
+                case 07:
+                    System.out.println("=== les montants total retirés ===");
+                    break;
+                case 8:
+                    System.out.println("Déconnexion réussie. À bientôt!");
+                    continuer=false;
                 default:
                     System.out.println("Choix invalide. Veuillez réessayer.");
+                    break;
             }
         }
     }
